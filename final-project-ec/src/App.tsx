231707fe150d08187c0a3c37c460725b9ae1cbc8
MartinaGV1 <mgarciavior@gmail.com>
2023-11-03 10:18:36 -0300

    create section structure

--- final-project-ec/src/App.tsx
+++ final-project-ec/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NewGame from './pages/newgame/NewGame';
 import Homepage from './pages/homepage/Homepage';
 import Perfil from './pages/perfil/Perfil';
 import NotFound from './pages/not-found/NotFound';
@@ -8,48 +9,56 @@ import NotFound from './pages/not-found/NotFound';
 
 const App: React.FC = () => {
 
-  const Layout = () => {
-    return (
-      <div className="container-app">
-        <h1 className="title">FINAL PROJECT</h1>
+  // const Layout = () => {
+  //   return (
+  //     <div className="container-app">
+  //       <h1 className="title">FINAL PROJECT</h1>
 
-        <div className="window">
-          <Outlet />
-        </div>
+  //       <div className="window">
+  //         <Outlet />
+  //       </div>
 
-      </div>
-    );
-  };
+  //     </div>
+  //   );
+  // };
 
-  const router = createBrowserRouter([
-    {
-      path: '/', 
-      element: <Layout />,
-      children: [
-        {
-          path: '/',
-          element: 
-            <>
-             <Homepage/>
-            </>
-        },
-        {
-          path: '/perfil',
-          element: <Perfil/>
-        },
-      ]
-    },
-    {
-      path: '/*',
-      element: <NotFound />
-    }
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: '/', 
+  //     element: <Layout />,
+  //     children: [
+  //       {
+  //         path: '/',
+  //         element: 
+  //           <>
+  //            <Homepage/>
+  //           </>
+  //       },
+  //       {
+  //         path: '/perfil',
+  //         element: <Perfil/>
+  //       },
+  //       {
+  //         path: '/newgame',
+  //         element: <NewGame/>
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     path: '/*',
+  //     element: <NotFound />
+  //   }
+  // ]);
+
+  // return (
+  //   <div>
+  //     <RouterProvider router={router} />
+  //   </div>
+  // );
 
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
+    <NewGame/>
+  )
 };
 
 export default App;
---
+++ final-project-ec/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>© All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer;
