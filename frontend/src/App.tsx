2888beb36ace0f6243c276498aae58a7ffc0d699
Daniela Fernandez Laura <dfernandezl@fi.uba.ar>
2023-11-19 23:35:19 -0300

    Modificacion de estilos para el buen funcionamiento del responsive

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-import './app.css';
-
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Profile from './pages/profile/profile';
-=======
 import './App.css';
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
->>>>>>> origin/main
 import NotFound from './pages/not-found/NotFound';
 import PrincipalPage from './pages/principal-page/PrincipalPage';
 import Nav from './components/nav/Nav';
 import Footer from './components/footer/Footer';
 import Register from './pages/register/Register';
-import { ICharacter } from './interfaces&types/ICharacter';
-import { TUser } from './interfaces&types/TUser';
-import UrlImage from './assets/user.jpeg'
-
-// Ejemplo
-const exampleCharacter: ICharacter= {
-  image: UrlImage,
-  name: "David",
-  sanity: "Alive",
-  state: true,
-  age: 16,
-  relatives: "",
-  ending: "End 1",
-};
-
-const exampleUser: TUser = {
-  username: "Apolo123",
-  email: "apol@gmail.com",
-  age: 13,
-  unlocked_characters: 2,
-  unlocked_endings: 1,
-  unlocked_objects: 3,
-  times_played: 10,
-  member_since: 2,
-};
-
 
 import Landing from './pages/landing/Landing';
 import Login from './pages/login/Login';
@@ -74,16 +41,6 @@ const App: React.FC = () => {
       children: [
         {
           path: '/',
-<<<<<<< HEAD
-          element: 
-            <>
-              <PrincipalPage/>
-            </>
-        },
-        {
-          path: '/profile',
-          element: <Profile userCharacter={exampleCharacter} playerProfile={exampleUser}/>
-=======
           element: <Landing />
         },
         {
@@ -108,7 +65,6 @@ const App: React.FC = () => {
         {
           path: '/home/profile',
           element: <Profile/>
->>>>>>> origin/main
         },
       ]
     },
--- frontend/src/pages/profile/profile.tsx
+++ frontend/src/pages/profile/profile.tsx
@@ -2,31 +2,55 @@ import React from "react";
 import './profile.scss'
 import { ICharacter } from "../../interfaces&types/ICharacter";
 import { TUser } from "../../interfaces&types/TUser";
+import UrlImage from '../../assets/user.jpeg';
 
-const Perfil: React.FC <{userCharacter: ICharacter, playerProfile: TUser}> = ({userCharacter, playerProfile}) => {
-  if (!userCharacter && !playerProfile) {
+const Perfil: React.FC = () => {
+  /*if (!userCharacter && !playerProfile) {
     return (
       <div>
         <p>Error en la carga de datos</p>
       </div>
     )
   }
+  */
+
+  // Ejemplo
+const Character: ICharacter= {
+  image: UrlImage,
+  name: "David",
+  sanity: "Alive",
+  state: true,
+  age: 16,
+  relatives: "",
+  ending: "End 1",
+};
+
+const User: TUser = {
+  username: "Apolo123",
+  email: "apol@gmail.com",
+  age: 13,
+  unlocked_characters: 2,
+  unlocked_endings: 1,
+  unlocked_objects: 3,
+  times_played: 10,
+  member_since: 2,
+};
 
   return (
     <div className="container_profile">
 
     <main>
-    <div className="container_img" style={{ backgroundImage: `url(${userCharacter.image})` }}></div>
+    <div className="container_img"  style={{ backgroundImage: `url(${Character.image})` }}></div> 
 
       <div className="container_cards">
         <div className="card_profile card">
           <h2 className="card_title" >PROFILE</h2>
           <ul className="card_list_data">
-            <li className="card_sub_title">USERNAME: <span>{ playerProfile.username }</span></li>
-            <li className="card_sub_title">AGE: <span>{ playerProfile.age }</span></li>
-            <li className="card_sub_title">UNLOCKED CHARACTERS: <span>{ playerProfile.unlocked_characters}</span></li>
-            <li className="card_sub_title">DAYS PLAYING: <span>{ playerProfile.times_played}</span></li>
-            <li className="playtime">Member since: { playerProfile.member_since } months</li>
+            <li className="card_sub_title">USERNAME: <span>{ User.username }</span></li>
+            <li className="card_sub_title">AGE: <span>{ User.age }</span></li>
+            <li className="card_sub_title">UNLOCKED CHARACTERS: <span>{ User.unlocked_characters}</span></li>
+            <li className="card_sub_title">DAYS PLAYING: <span>{ User.times_played}</span></li>
+            <li className="playtime">Member since: { User.member_since } months</li>
           </ul>
           
         </div>
@@ -34,10 +58,10 @@ const Perfil: React.FC <{userCharacter: ICharacter, playerProfile: TUser}> = ({u
         <div className="card_character card">
           <h2 className="card_title">CHARACTER DATA</h2>
           <ul className="card_list_data">
-            <li className="card_sub_title">NAME: <span>{ userCharacter.name }</span></li>
-            <li className="card_sub_title">SANITY: <span>{ userCharacter.sanity }</span></li>
-            <li >{userCharacter.state? (<p className="card_sub_title">STATE: <span>Alive</span></p>):(<p className="card_sub_title">STATE: <span>Dead</span></p>)}</li>
-            <li className="card_sub_title">ENDING: <span>{ userCharacter.ending }</span></li>
+            <li className="card_sub_title">NAME: <span>{ Character.name }</span></li>
+            <li className="card_sub_title">SANITY: <span>{ Character.sanity }</span></li>
+            <li >{Character.state? (<p className="card_sub_title">STATE: <span>Alive</span></p>):(<p className="card_sub_title">STATE: <span>Dead</span></p>)}</li>
+            <li className="card_sub_title">ENDING: <span>{ Character.ending }</span></li>
           </ul>
         </div>
       </div>
