7572239548a83034eb404a5cfc98349dbe7c7fca
Daniela Fernandez Laura <dfernandezl@fi.uba.ar>
2023-11-29 15:33:16 -0300

    Agrego un dropdown para la interaccion con las paginas Profile y Unlocked Characters

--- frontend/src/components/nav/Nav.tsx
+++ frontend/src/components/nav/Nav.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import './nav.scss';
 import { NavLink, useLocation } from 'react-router-dom';
 import { FaCircleUser } from 'react-icons/fa6';
+import { BsCaretDownFill } from "react-icons/bs";
+import { TUser } from "../../interfaces&types/TUser";
 
 const Nav: React.FC = () => {
+// A modo de ejemplo
+
+    const User: TUser = {
+        id: 1,
+        username: "Apolo123",
+        email: "apol@gmail.com",
+        age: 13,
+        unlocked_characters: 2,
+        unlocked_endings: 1,
+        unlocked_objects: 3,
+        times_played: 10,
+        member_since: 2,
+      };
 
     const location = useLocation();
     const isProfileOrCharacters =
@@ -21,6 +36,14 @@ const Nav: React.FC = () => {
         console.log('User Logged Out')
     };
 
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const handleToggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+
   return (
     <div className={`container-nav-absolute ${isProfileOrCharacters ? 'white-nav' : ''}`}>
         <div className="container-nav">
@@ -36,6 +59,16 @@ const Nav: React.FC = () => {
                 <NavLink to="/home/profile" className="user-icon-container">
                     <FaCircleUser className="user-icon"/>
                 </NavLink>
+            
+                <div className={`dropdown-container ${isDropdownOpen ? 'show' : ''}`} onClick={handleToggleDropdown}>
+                        <BsCaretDownFill className="user-bar" />
+                        <ul>
+                            <li className="user-name">Welcome <span>{User.username}</span> !</li>
+                            <li><NavLink to="/home/profile" className="opc-bar">Profile</NavLink></li>
+                            <li><NavLink to="/home/characters" className="opc-bar">Unlocked characters</NavLink></li>
+                        </ul>
+                </div>
+                
             </div>
         </div>
     </div>
